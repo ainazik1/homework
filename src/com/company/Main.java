c186b019167c5f93a40441a7df0f828ec4ab8b45
ainazik1 <ainazikzamirkyzy@gmail.com>
2021-11-10 15:59:38 +0300

    dd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        String myAinazik;
+        final int NUM = 21;
+        String word = "День рождения";
+        myAinazik = NUM + word;
+        System.out.println(myAinazik);
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили нуль");
+        }
     }
 }
